97c60b8dad6e903d0aa02f8c344735613ce16452
Nanda <nandayavanets@mail.ru>
2024-01-10 22:10:08 +0700

    delete js

--- main.js
+++
@@ -1,7 +0,0 @@
-const content = document.getElementsByClassName('content');
-
-for (i=0; i<content.length; i++) {
-  content[i].addEventListener('click', function () {
-    this.classList.toggle('active')
-  })
-}
